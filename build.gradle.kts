a9b6a3ecce08ab0539167bedc2a34677de459cc1
Rahul Behera <thebehera@fb.com>
2021-12-25 13:25:05 -0800

    use latest buffer module

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("com.ditchoom:buffer:1.0.50")
+                implementation("com.ditchoom:buffer:1.0.51")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
             }
         }
