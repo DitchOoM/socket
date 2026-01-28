1a5f42feacf6d2d43f9c49618076fcac0680d18e
Rahul Behera <rbehera@gmail.com>
2026-01-28 13:10:43 -0600

    Fix ktlint formatting in LinuxTlsTests

--- src/linuxTest/kotlin/com/ditchoom/socket/LinuxTlsTests.kt
+++ src/linuxTest/kotlin/com/ditchoom/socket/LinuxTlsTests.kt
@@ -8,23 +8,23 @@ import kotlin.time.Duration.Companion.seconds
  * Linux-specific TLS tests.
  */
 class LinuxTlsTests {
-
     @Test
-    fun tlsConnectionRequiresCACertificates() = runTestNoTimeSkipping {
-        // Verify that TLS connections work (implicitly tests CA loading succeeds)
-        // If CA loading threw an exception, this test would fail
-        ClientSocket.connect(
-            port = 443,
-            hostname = "www.google.com",
-            tls = true,
-            timeout = 10.seconds,
-        ) { socket ->
-            assertTrue(socket.isOpen(), "TLS socket should be open")
-            socket.writeString("GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n")
-            val response = socket.readString(timeout = 5.seconds)
-            assertTrue(response.startsWith("HTTP/"), "Should receive HTTP response over TLS")
+    fun tlsConnectionRequiresCACertificates() =
+        runTestNoTimeSkipping {
+            // Verify that TLS connections work (implicitly tests CA loading succeeds)
+            // If CA loading threw an exception, this test would fail
+            ClientSocket.connect(
+                port = 443,
+                hostname = "www.google.com",
+                tls = true,
+                timeout = 10.seconds,
+            ) { socket ->
+                assertTrue(socket.isOpen(), "TLS socket should be open")
+                socket.writeString("GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n")
+                val response = socket.readString(timeout = 5.seconds)
+                assertTrue(response.startsWith("HTTP/"), "Should receive HTTP response over TLS")
+            }
         }
-    }
 
     @Test
     fun sslSocketExceptionContainsUsefulMessage() {
