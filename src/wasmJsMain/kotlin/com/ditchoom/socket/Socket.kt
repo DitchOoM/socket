50376065535a565d00d007b97b62252d2f6aed93
Rahul Behera <rbehera@gmail.com>
2026-03-24 15:33:24 -0500

    Fix ktlint: wasmJs source formatting

--- src/wasmJsMain/kotlin/com/ditchoom/socket/Socket.kt
+++ src/wasmJsMain/kotlin/com/ditchoom/socket/Socket.kt
@@ -3,5 +3,4 @@ package com.ditchoom.socket
 actual fun ClientSocket.Companion.allocate(): ClientToServerSocket =
     throw UnsupportedOperationException("Sockets are not supported in WASM")
 
-actual fun ServerSocket.Companion.allocate(): ServerSocket =
-    throw UnsupportedOperationException("Sockets are not supported in WASM")
+actual fun ServerSocket.Companion.allocate(): ServerSocket = throw UnsupportedOperationException("Sockets are not supported in WASM")
